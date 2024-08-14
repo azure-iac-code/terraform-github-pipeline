7e6a5a312fd81e1b48d8cefcbadbf69a0e65d40e
Marcus <m.barbosa.fernandes@avanade.com>
2024-08-13 23:15:22 -0300

    Ajuste33

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,67 @@
-module "tags_finops" {
-  source        = "git::https://github.com/Bradesco-IAC/trf-azu-tags-finops.git?ref=develop"
-  centro_custo  = var.centro_custo
-  empresa       = var.empresa
-  departamento  = var.departamento
-  app           = var.app
-  componente    = var.componente
-  ambiente      = var.ambiente
-  bo            = var.bo
-  to            = var.to
-  gerenciamento = var.gerenciamento
-  provedor      = var.provedor
-  dominio       = var.dominio
+data "azurerm_resource_group" "rg_keyvault" {
+  name = var.resource_group_name
 }
 
-module "resource_group" {
-  source                          = "git::https://github.com/Bradesco-IAC/trf-azu-resource-group.git?ref=V1"
-  azurerm_resource_group_name     = var.azurerm_key_vault_resource_group_name
-  azurerm_resource_group_location = var.azurerm_resource_group_location
-  tags                            = module.tags_finops.tags_finops
+data "azurerm_virtual_network" "vnet_keyvault" {
+  name                = var.virtual_network_name
+  resource_group_name = var.resource_group_name
 }
 
-module "vnet" {
-  source                                = "git::https://github.com/Bradesco-IAC/trf-azu-vnet.git?ref=V2"
-  azurerm_resource_group_name           = var.azurerm_key_vault_resource_group_name
-  azurerm_virtual_network_location      = var.azurerm_virtual_network_location
-  azurerm_virtual_network_name          = var.subnet_virtual_network_name
-  azurerm_virtual_network_address_space = var.azurerm_virtual_network_address_space
-  azurerm_subnet_address_prefixes       = var.azurerm_subnet_address_prefixes
-  azurerm_subnet_name                   = var.azurerm_subnet_name
-  ambiente                              = var.ambiente
-  depends_on                            = [module.resource_group]
+data "azurerm_subnet" "subnet_keyvault" {
+  name                 = var.azurerm_subnet_keyvault_name
+  virtual_network_name = var.virtual_network_name
+  resource_group_name  = var.resource_group_name
 }
 
-resource "azurerm_subnet" "subnet_jump" {
-  name                 = var.jump_subnet_name
-  resource_group_name  = var.azurerm_key_vault_resource_group_name
-  virtual_network_name = var.subnet_virtual_network_name
-  address_prefixes     = ["172.30.2.0/24"]
-  depends_on           = [module.resource_group]
+data "azurerm_subnet" "subnet_jump" {
+  name                 = var.azurerm_subnet_jump_name
+  virtual_network_name = var.virtual_network_name
+  resource_group_name  = var.resource_group_name
+
 }
 
-data "azurerm_client_config" "current" {}
+#data "azurerm_client_config" "current" {}
+
+
+
+# module "tags_finops" {
+#   source        = "git::https://github.com/azure-iac-code/trf-azu-keyvault.git"
+#   centro_custo  = var.centro_custo
+#   empresa       = var.empresa
+#   departamento  = var.departamento
+#   app           = var.app
+#   componente    = var.componente
+#   ambiente      = var.ambiente
+#   bo            = var.bo
+#   to            = var.to
+#   gerenciamento = var.gerenciamento
+#   provedor      = var.provedor
+#   dominio       = var.dominio
+# }
+
+# module "resource_group" {
+#   source                          = "git::https://github.com/azure-iac-code/trf-azu-keyvault.git"
+#   resource_group_name     = var.azurerm_key_vault_resource_group_name
+#   azurerm_resource_group_location = var.azurerm_resource_group_location
+#   tags                            = module.tags_finops.tags_finops
+# }
+
+# module "vnet" {
+#   source                                = "git::https://github.com/azure-iac-code/trf-azu-keyvault.git"
+#   resource_group_name           = var.azurerm_key_vault_resource_group_name
+#   azurerm_virtual_network_location      = var.azurerm_virtual_network_location
+#   virtual_network_name          = var.subnet_virtual_network_name
+#   azurerm_virtual_network_address_space = var.azurerm_virtual_network_address_space
+#   azurerm_subnet_address_prefixes       = var.azurerm_subnet_address_prefixes
+#   azurerm_subnet_name                   = var.azurerm_subnet_name
+#   ambiente                              = var.ambiente
+#   depends_on                            = [module.resource_group]
+# }
 
 module "key_vault" {
-  source                 = "git::https://github.com/Bradesco-IAC/trf-azu-key-vault.git?ref=develop"
-  azurerm_key_vault_name = var.azurerm_key_vault_name
-  keyvault_subnet_name   = var.keyvault_subnet_name
-  ambiente               = var.ambiente
-  #subnet_name                   = var.subnet_name
+  source                                = "git::https://github.com/azure-iac-code/trf-azu-keyvault.git"
+  azurerm_key_vault_name                = var.azurerm_key_vault_name
+  ambiente                              = var.ambiente
   subnet_resource_group_name            = var.subnet_resource_group_name
   azurerm_key_vault_resource_group_name = var.azurerm_key_vault_resource_group_name
   subnet_virtual_network_name           = var.subnet_virtual_network_name
@@ -60,12 +74,12 @@ module "key_vault" {
   purge_protection_enabled              = var.purge_protection_enabled
   public_network_access_enabled         = var.public_network_access_enabled
 
-  #################################################################################
+
+  ##################################################################################
   #                                  PRIVATE ENDPOINT
-  #################################################################################
-  private_service_connection_name = var.private_service_connection_name
-  azurerm_private_dns_zone_name   = var.azurerm_private_dns_zone_name
-  azurerm_private_endpoint_name   = var.azurerm_private_endpoint_name #"pvtkvazu${var.ambiente}bra${var.azurerm_key_vault_name}"
+  ##################################################################################
+  #private_service_connection_name = var.private_service_connection_name
+  azurerm_private_dns_zone_name = var.azurerm_private_dns_zone_name
 
   ####################################################################################
   #                                DIAGNOSTIC SETTINGS
@@ -75,13 +89,13 @@ module "key_vault" {
   namespace_name                                                    = var.namespace_name
 
 
-  azurerm_monitor_diagnostic_setting_name = var.azurerm_monitor_diagnostic_setting_name #"${var.azurerm_key_vault_name}_diagnostic"
+  azurerm_monitor_diagnostic_setting_name = "${var.azurerm_key_vault_name}_diagnostic"
   eventhub_name                           = var.eventhub_name
-  depends_on                              = [azurerm_subnet.subnet_jump]
+  depends_on                              = [module.vnet]
 
-  providers = {
-    azurerm                   = azurerm
-    azurerm.ditiidentity      = azurerm.ditiidentity
-    azurerm.ditigerenciamento = azurerm.ditigerenciamento
-  }
+  #   providers = {
+  #     azurerm                   = azurerm
+  #     azurerm.ditiidentity      = azurerm.ditiidentity
+  #     azurerm.ditigerenciamento = azurerm.ditigerenciamento
+  #   }
 }
---
+++ terraform/terraform.dev.tfvars
@@ -0,0 +1,14 @@
+#---------------------
+#     RESOURCE GROUP
+#---------------------
+resource_group_name = "rg-keyvault"
+
+
+virtual_network_name = "vnet-keyvault"
+
+azurerm_subnet_keyvault_name = "subnet-keyvault"
+
+azurerm_subnet_jump_name = "subnet-jump"
+
+
+
