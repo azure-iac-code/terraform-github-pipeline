9878c5af1d66e16968c95ec23929ca28782aab9b
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-16 19:07:57 -0300

    Ajuste Provider

--- terraform/required.tf
+++ terraform/required.tf
@@ -1,10 +1,19 @@
-provider "azurerm" {
-  features {}
-  use_msi = true
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>3.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+#   use_msi = true
+# }
 
 terraform {
-  backend "storage_account" {
+  backend "azurerm" {
     resource_group_name  = "rg-runner"
     storage_account_name = "terraformdvbra"
     container_name       = "tfstate"
