d1d50a227d1d195e13de46eb3e79fb45fa416076
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-16 23:09:41 -0300

    Alterando plan na pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
 
 data "azurerm_resource_group" "this" {
   name     = "rg-keyvault"
