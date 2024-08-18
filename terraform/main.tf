2cebcb17fad71616d0ae9790014a932f65b1508e
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-18 13:54:00 -0300

    Adicioannvo recurso keyvault

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ resource "azurerm_private_endpoint" "this" {
 resource "azurerm_key_vault_access_policy" "this" {
   key_vault_id            = azurerm_key_vault.this.id
   tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = azurerm_key_vault_access_policy.this.id
+  object_id               = azurerm_key_vault_access_policy.this.object_id
   key_permissions         = ["Get", "List"]
   secret_permissions      = ["Get", "List"]
   certificate_permissions = ["Get", "List"]
