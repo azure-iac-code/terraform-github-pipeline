68ce5c773e7ef4a49524d48f6c909d9226d36b95
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-18 14:19:49 -0300

    Adicionando depends_onclient_config

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,6 +91,7 @@ resource "azurerm_private_endpoint" "this" {
 }
 
 resource "azurerm_key_vault_access_policy" "this" {
+  depends_on = [azurerm_client_config.current]
   key_vault_id            = azurerm_key_vault.this.id
   tenant_id               = data.azurerm_client_config.current.tenant_id
   object_id               = data.azurerm_client_config.current.object_id
