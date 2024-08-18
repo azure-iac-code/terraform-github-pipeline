624f86b0a8c5a680963938821aebc6e2d03b1b4a
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-18 17:36:01 -0300

    Adicionando eventhub namespace

--- terraform/main.tf
+++ terraform/main.tf
@@ -180,8 +180,8 @@ resource "azurerm_eventhub_namespace_authorization_rule" "this" {
 
 resource "azurerm_eventhub" "this" {
   name                = var.eventhub_name
-  namespace_name      = azurerm_eventhub_namespace.this.name
-  resource_group_name = azurerm_resource_group.this.name
+  namespace_name      = var.namespace_name #azurerm_eventhub_namespace.this.name
+  resource_group_name = var.azurerm_eventhub_namespace_authorization_rule_resource_group_name
   partition_count     = 2
   message_retention   = 1
 }
