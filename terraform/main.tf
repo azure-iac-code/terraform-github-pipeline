98446f5d8dd2968f0e5df3c0f7d58bf68ca2aa78
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-18 20:03:37 -0300

    Destroy everithing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,152 +1,152 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.116.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-data "azurerm_client_config" "current" {
-}
-
-data "azurerm_resource_group" "this" {
-  name = "rg-keyvault"
-}
-
-data "azurerm_virtual_network" "this" {
-  name                = "vnet-keyvault"
-  resource_group_name = data.azurerm_resource_group.this.name
-  depends_on          = [data.azurerm_resource_group.this]
-}
-
-data "azurerm_subnet" "keyvault_subnet" {
-  name                 = "keyvault_subnet"
-  resource_group_name  = data.azurerm_resource_group.this.name
-  virtual_network_name = data.azurerm_virtual_network.this.name
-  depends_on           = [data.azurerm_resource_group.this]
-}
-
-data "azurerm_subnet" "jump_subnet" {
-  name                 = "jump_subnet"
-  resource_group_name  = data.azurerm_resource_group.this.name
-  virtual_network_name = data.azurerm_virtual_network.this.name
-  depends_on           = [data.azurerm_resource_group.this]
-}
-
-resource "azurerm_key_vault" "this" {
-  name                            = var.azurerm_key_vault_name
-  location                        = var.azurerm_key_vault_location
-  resource_group_name             = var.azurerm_key_vault_resource_group_name
-  sku_name                        = var.sku_name
-  tenant_id                       = data.azurerm_client_config.current.tenant_id
-  enabled_for_deployment          = var.enabled_for_deployment
-  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
-  enabled_for_template_deployment = var.enabled_for_template_deployment
-  enable_rbac_authorization       = var.enable_rbac_authorization
-  soft_delete_retention_days      = var.soft_delete_retention_days
-  purge_protection_enabled        = var.purge_protection_enabled
- 
-  network_acls {
-    bypass                     = var.network_acls_bypass
-    default_action             = var.network_acls_default_action
-    virtual_network_subnet_ids = [data.azurerm_subnet.keyvault_subnet.id, data.azurerm_subnet.jump_subnet.id]
-    ip_rules                   = var.network_acls_ip_rules
-  }
-}
-
-#----------------------------------------
-# Recurso privateEndPoint
-#----------------------------------------
-resource "azurerm_private_endpoint" "this" {
-  depends_on          = [azurerm_key_vault.this]
-  name                = "pvtkvazu${var.ambiente}bra${var.azurerm_key_vault_name}"
-  resource_group_name = var.azurerm_key_vault_resource_group_name
-  location            = var.azurerm_key_vault_location
-  subnet_id           = data.azurerm_subnet.keyvault_subnet.id
-
-  private_service_connection {
-    name                           = "pvtcn${var.ambiente}bra${var.azurerm_key_vault_name}"
-    private_connection_resource_id = azurerm_key_vault.this.id
-    is_manual_connection           = var.private_service_connection_is_manual_connection
-    subresource_names              = ["vault"]
-  }
-}
-
-resource "azurerm_key_vault_access_policy" "this" {
-  key_vault_id            = azurerm_key_vault.this.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = data.azurerm_client_config.current.object_id
-  key_permissions         = ["Get", "List"]
-  secret_permissions      = ["Get", "List"]
-  certificate_permissions = ["Get", "List"]
-  depends_on              = [data.azurerm_client_config.current]
-}
-
-resource "azurerm_role_assignment" "key_vault" {
-    count                = 4
-    scope                = "/subscriptions/${var.subscription_id}/resourceGroups/${var.azurerm_key_vault_resource_group_name}/providers/Microsoft.KeyVault/vaults/${var.azurerm_key_vault_name}"
-    role_definition_name = element(["Key Vault Contributor", "Key Vault Reader", "Security Admin", "Security Reader"], count.index)
-    principal_id         = var.azurerm_role_assignment_principal_id
-}
-
-# resource "azurerm_role_assignment" "this" {
-#   scope                = azurerm_key_vault.this.id
-#   role_definition_name = "Key Vault Contributor"
-#   principal_id         = data.azurerm_client_config.current.object_id
-# }
-
-# resource "azurerm_role_assignment" "this" {
-#   scope                = azurerm_key_vault.this.id
-#   role_definition_name = "Key Vault Reader"
-#   principal_id         = data.azurerm_client_config.current.object_id
-# }
-
-# resource "azurerm_role_assignment" "this" {
-#   scope                = azurerm_key_vault.this.id
-#   role_definition_name = "Security Admin"
-#   principal_id         = data.azurerm_client_config.current.object_id
-# }
-
-# resource "azurerm_role_assignment" "this" {
-#   scope                = azurerm_key_vault.this.id
-#   role_definition_name = "Security Reader"
-#   principal_id         = data.azurerm_client_config.current.object_id
-# }
-
-resource "azurerm_eventhub" "this" {
-  name                = var.eventhub_name
-  namespace_name      = var.namespace_name
-  resource_group_name = var.azurerm_eventhub_namespace_authorization_rule_resource_group_name
-  partition_count     = 2
-  message_retention   = 1
-}
-
-# data "azurerm_eventhub_namespace_authorization_rule" "this" {
-#   name                = var.azurerm_eventhub_namespace_authorization_rule_name
-#   resource_group_name = var.azurerm_eventhub_namespace_authorization_rule_resource_group_name
-#   namespace_name      = var.namespace_name
-#   #  provider            = azurerm.ditigerenciamento
-# }
-
-# resource "azurerm_eventhub_consumer_group" "this" {
-#   name                = "this-consumer-group"
-#   eventhub_name       = azurerm_eventhub.this.name
-#   namespace_name      = var.namespace_name
-#   resource_group_name = azurerm_resource_group.this.name
-# }
-
-# resource "azurerm_monitor_diagnostic_setting" "this" {
-#   name                           = "${var.azurerm_key_vault_name}_diagnostic"
-#   target_resource_id             = azurerm_key_vault.this.id
-#   eventhub_name                  = var.eventhub_name
-#   eventhub_authorization_rule_id = data.azurerm_eventhub_namespace_authorization_rule.this.id
-
-#   enabled_log {
-#     category_group = "audit"
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "3.116.0"
+#     }
 #   }
 # }
+
+# provider "azurerm" {
+#   features {}
+# }
+
+# data "azurerm_client_config" "current" {
+# }
+
+# data "azurerm_resource_group" "this" {
+#   name = "rg-keyvault"
+# }
+
+# data "azurerm_virtual_network" "this" {
+#   name                = "vnet-keyvault"
+#   resource_group_name = data.azurerm_resource_group.this.name
+#   depends_on          = [data.azurerm_resource_group.this]
+# }
+
+# data "azurerm_subnet" "keyvault_subnet" {
+#   name                 = "keyvault_subnet"
+#   resource_group_name  = data.azurerm_resource_group.this.name
+#   virtual_network_name = data.azurerm_virtual_network.this.name
+#   depends_on           = [data.azurerm_resource_group.this]
+# }
+
+# data "azurerm_subnet" "jump_subnet" {
+#   name                 = "jump_subnet"
+#   resource_group_name  = data.azurerm_resource_group.this.name
+#   virtual_network_name = data.azurerm_virtual_network.this.name
+#   depends_on           = [data.azurerm_resource_group.this]
+# }
+
+# resource "azurerm_key_vault" "this" {
+#   name                            = var.azurerm_key_vault_name
+#   location                        = var.azurerm_key_vault_location
+#   resource_group_name             = var.azurerm_key_vault_resource_group_name
+#   sku_name                        = var.sku_name
+#   tenant_id                       = data.azurerm_client_config.current.tenant_id
+#   enabled_for_deployment          = var.enabled_for_deployment
+#   enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+#   enabled_for_template_deployment = var.enabled_for_template_deployment
+#   enable_rbac_authorization       = var.enable_rbac_authorization
+#   soft_delete_retention_days      = var.soft_delete_retention_days
+#   purge_protection_enabled        = var.purge_protection_enabled
+ 
+#   network_acls {
+#     bypass                     = var.network_acls_bypass
+#     default_action             = var.network_acls_default_action
+#     virtual_network_subnet_ids = [data.azurerm_subnet.keyvault_subnet.id, data.azurerm_subnet.jump_subnet.id]
+#     ip_rules                   = var.network_acls_ip_rules
+#   }
+# }
+
+# #----------------------------------------
+# # Recurso privateEndPoint
+# #----------------------------------------
+# resource "azurerm_private_endpoint" "this" {
+#   depends_on          = [azurerm_key_vault.this]
+#   name                = "pvtkvazu${var.ambiente}bra${var.azurerm_key_vault_name}"
+#   resource_group_name = var.azurerm_key_vault_resource_group_name
+#   location            = var.azurerm_key_vault_location
+#   subnet_id           = data.azurerm_subnet.keyvault_subnet.id
+
+#   private_service_connection {
+#     name                           = "pvtcn${var.ambiente}bra${var.azurerm_key_vault_name}"
+#     private_connection_resource_id = azurerm_key_vault.this.id
+#     is_manual_connection           = var.private_service_connection_is_manual_connection
+#     subresource_names              = ["vault"]
+#   }
+# }
+
+# resource "azurerm_key_vault_access_policy" "this" {
+#   key_vault_id            = azurerm_key_vault.this.id
+#   tenant_id               = data.azurerm_client_config.current.tenant_id
+#   object_id               = data.azurerm_client_config.current.object_id
+#   key_permissions         = ["Get", "List"]
+#   secret_permissions      = ["Get", "List"]
+#   certificate_permissions = ["Get", "List"]
+#   depends_on              = [data.azurerm_client_config.current]
+# }
+
+# resource "azurerm_role_assignment" "key_vault" {
+#     count                = 4
+#     scope                = "/subscriptions/${var.subscription_id}/resourceGroups/${var.azurerm_key_vault_resource_group_name}/providers/Microsoft.KeyVault/vaults/${var.azurerm_key_vault_name}"
+#     role_definition_name = element(["Key Vault Contributor", "Key Vault Reader", "Security Admin", "Security Reader"], count.index)
+#     principal_id         = var.azurerm_role_assignment_principal_id
+# }
+
+# # resource "azurerm_role_assignment" "this" {
+# #   scope                = azurerm_key_vault.this.id
+# #   role_definition_name = "Key Vault Contributor"
+# #   principal_id         = data.azurerm_client_config.current.object_id
+# # }
+
+# # resource "azurerm_role_assignment" "this" {
+# #   scope                = azurerm_key_vault.this.id
+# #   role_definition_name = "Key Vault Reader"
+# #   principal_id         = data.azurerm_client_config.current.object_id
+# # }
+
+# # resource "azurerm_role_assignment" "this" {
+# #   scope                = azurerm_key_vault.this.id
+# #   role_definition_name = "Security Admin"
+# #   principal_id         = data.azurerm_client_config.current.object_id
+# # }
+
+# # resource "azurerm_role_assignment" "this" {
+# #   scope                = azurerm_key_vault.this.id
+# #   role_definition_name = "Security Reader"
+# #   principal_id         = data.azurerm_client_config.current.object_id
+# # }
+
+# resource "azurerm_eventhub" "this" {
+#   name                = var.eventhub_name
+#   namespace_name      = var.namespace_name
+#   resource_group_name = var.azurerm_eventhub_namespace_authorization_rule_resource_group_name
+#   partition_count     = 2
+#   message_retention   = 1
+# }
+
+# # data "azurerm_eventhub_namespace_authorization_rule" "this" {
+# #   name                = var.azurerm_eventhub_namespace_authorization_rule_name
+# #   resource_group_name = var.azurerm_eventhub_namespace_authorization_rule_resource_group_name
+# #   namespace_name      = var.namespace_name
+# #   #  provider            = azurerm.ditigerenciamento
+# # }
+
+# # resource "azurerm_eventhub_consumer_group" "this" {
+# #   name                = "this-consumer-group"
+# #   eventhub_name       = azurerm_eventhub.this.name
+# #   namespace_name      = var.namespace_name
+# #   resource_group_name = azurerm_resource_group.this.name
+# # }
+
+# # resource "azurerm_monitor_diagnostic_setting" "this" {
+# #   name                           = "${var.azurerm_key_vault_name}_diagnostic"
+# #   target_resource_id             = azurerm_key_vault.this.id
+# #   eventhub_name                  = var.eventhub_name
+# #   eventhub_authorization_rule_id = data.azurerm_eventhub_namespace_authorization_rule.this.id
+
+# #   enabled_log {
+# #     category_group = "audit"
+# #   }
+# # }
