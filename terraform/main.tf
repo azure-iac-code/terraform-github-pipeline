3615232f0221138d1a15fe8e1f32ec5658dab863
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-29 17:19:30 -0300

    Changing pipeline yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,13 @@ data "azurerm_resource_group" "rg_terraform" {
 
 data "azurerm_virtual_network" "vnet_terraform" {
   name                = var.azurerm_virtual_network_name
-  resource_group_name = data.azurerm_resource_group.rg_terraform.name
+  resource_group_name = data.azurerm_resource_group_name
   #depends_on          = [data.azurerm_resource_group.this]
 }
 
 data "azurerm_subnet" "subnet_terraform" {
   name                 = "subnet-terraform-modules"
-  resource_group_name  = data.azurerm_resource_group.rg_terraform.name
+  resource_group_name  = data.azurerm_resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_terraform.name
   #depends_on           = [data.azurerm_resource_group.this]
 }
@@ -18,7 +18,7 @@ data "azurerm_subnet" "subnet_terraform" {
 resource "azurerm_subnet" "subnet_terraform_2" {
   name                 = var.azurerm_subnet
   address_prefixes     = var.azurerm_subnet_address_prefixes
-  resource_group_name  = data.azurerm_resource_group.rg_terraform.name
+  resource_group_name  = data.azurerm_resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet_terraform.name
 }
 
