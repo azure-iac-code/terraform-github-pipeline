f99c3ec8727ee76806f12b01296a3a8c25161837
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-08-29 15:28:57 -0300

    Destroying everything

---
+++ terraform/required.tf
@@ -0,0 +1,13 @@
+# provider "azurerm" {
+#   features {}
+#   use_msi = true
+# }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-runner"
+    storage_account_name = "terraformdvbra"
+    container_name       = "tfstate"
+    key                  = "key-github"
+  }
+}
