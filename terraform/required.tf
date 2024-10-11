3dfd55a02313ffc6998099bfe46876ee7df9ac50
marcusviniciusbf86 <marcusvinicius_86@outlook.com>
2024-10-11 11:11:02 -0300

    Ajuste Storage Account

--- terraform/required.tf
+++ terraform/required.tf
@@ -1,7 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.116.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  use_msi = true
 }
+
+# provider "azurerm" {
+#   features {}
+#   use_msi = true
+# }
 
 terraform {
   backend "azurerm" {
@@ -11,4 +24,3 @@ terraform {
     key                  = "key-github"
   }
 }
- 
